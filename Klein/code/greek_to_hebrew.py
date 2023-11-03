a18d9a6af83184e8655b98764d3934762674449c
Brayden K <brayk0hler@gmail.com>
2023-11-03 11:52:25 -0400

    Klein's Greek to Heb dict and combined dicts

---
+++ Klein/code/greek_to_hebrew.py
@@ -0,0 +1,62 @@
+ # NOTE: Useing regex instead of re to make variable width lookahead
+import regex
+import os
+import pandas as pd
+import unicodedata
+
+fileName = "Greek-Klein.csv"
+
+dir = os.path.dirname(__file__) + '/../data'
+filePath  = f"{dir}/{fileName}"
+
+greek_letters = regex.compile(r'''\b(?<=Gk. suff. -\s?|Greek suff. -\s?|Gk. suff. –\s?|Gk. –\s?|Gk. -\s?
+                              |Gk. \(pathos\)\s?|Gk. form\s?|Gk. from\s?|Gk. adjectives ending in – |Gk. vulgar var.|
+                              Gk.\s?)\w+(?<!suff|word|words|form|from|pref|adjectives|privative|vulgar)\b''',regex.X)
+
+def load_dfs(*filePaths):
+    output = []
+    for path in filePaths:
+        output.append(pd.read_csv(path))
+    return output if len(output)>1 else output[0]
+
+def get_greek_word(df):
+    greek_words= []
+    extra_rows = []
+    for idx in range(len(df)):
+        row = df.iloc[idx]
+        match = list(set( i for i in greek_letters.findall(row["Definition"]) if len(i)>1))
+        try:
+            greek_words.append(match[0].lower())
+        except:
+            greek_words.append("REMOVE")
+            continue
+        for m in match[1:]:
+            duplicated_row = row.copy()
+            duplicated_row["Greek Entry"] = m.lower()
+            extra_rows.append(duplicated_row)
+    print(f"Adding {len(extra_rows)} more rows than the original dict")
+    return greek_words, extra_rows
+
+def re_organize_df(df, greek_words,extra_rows):
+    df["Greek Entry"] = greek_words
+    df = df.drop(df[df["Greek Entry"] == "REMOVE"].index)
+    df = pd.concat([df,pd.DataFrame(extra_rows)],ignore_index=True)
+    df['SortKey'] = df["Greek Entry"].apply(lambda x: unicodedata.normalize('NFD', x))
+    df = df.sort_values(by="SortKey")
+    df.drop('SortKey', axis=1, inplace=True)
+    df = df.reindex(columns=['Unnamed: 0', 'Greek Entry', 'Entry', 'Definition'])
+    return df
+
+        
+            
+
+
+if __name__ == "__main__":
+    df = load_dfs(filePath)
+    greek_words, extra_rows = get_greek_word(df)
+    df = re_organize_df(df, greek_words,extra_rows)
+    df.to_csv(f"{dir}/Greek-Heb_Aram.csv", index=False)
+
+    for greek in df["Greek Entry"]:
+        if "h" in greek:
+            print(greek)
---
+++ Merge/code/merge_GreektoHeb.py
@@ -0,0 +1,104 @@
+import re
+import os
+import pandas as pd
+import unicodedata
+
+fileName = "Greek-Heb_Aram.csv"
+
+dir = os.path.dirname(__file__) + "/../.."
+filePath_0  = f"{dir}/Jastrow/data/01-Merged XML/{fileName}"
+filePath_1  = f"{dir}/Klein/data/{fileName}"
+#["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "x", "y", "z", "í", "ï", "ē", "ō"]
+
+def load_dfs(*filePaths):
+    output = []
+    for path in filePaths:
+        output.append(pd.read_csv(path))
+    return output if len(output)>1 else output[0]
+
+def tag_roman_word(word):
+    """
+    Adds a Greek letter to put the word into proper alpha-omega order.
+    Adds an em—dash to make sure the roman spelled words come after greek
+    spelled words.
+    """
+    replacements = {
+        "th": "θ",
+        "ph": "φ",
+        "ch": "χ",
+        "ps": "ψ"
+    }
+    for key in replacements.keys():
+        if word[:2] == key:
+            word = replacements[key] + word[2:]
+    lookup = {
+        "a": "α",
+        "b": "β",
+        "v": "β",
+        "g": "γ",
+        "d": "δ",
+        "e": "ε",
+        "ē": "ε",
+        "z": "ζ",
+        "i": "ι",
+        "í": "ι",
+        "ï": "ι",
+        "c": "κ",
+        "k": "κ",
+        "l": "λ",
+        "m": "μ",
+        "n": "ν",
+        "x": "ξ",
+        "o": "ο",
+        "p": "π",
+        "r": "ρ",
+        "s": "σ",
+        "t": "τ",
+        "u": "υ",
+        "y": "υ",
+        "ō": "ω",
+    }
+    if word[0] == "h":
+        return lookup[word[1]] + "—" + word[1:] + "<tag>h"
+
+    return lookup.get(word[0], word[0]) + "—" + word
+
+def untag_roman_word(word):
+    word = word[2:]
+    if word[-6:] == "<tag>h": # Has a tag
+        word = "h" + word[:-6]
+    replacements = {
+        "θ" : "th",
+        "φ" : "ph",
+        "χ" : "ch",
+        "ψ" : "ps"
+    }
+    for key in replacements.keys():
+        if word[0] == key:
+            word = replacements[key] + word[1:]
+
+    return word
+
+
+def sort_words(greekdf, romandf):
+    # Prepare Dicts
+    romandf["Greek Entry"] = romandf["Greek Entry"].apply(tag_roman_word)
+    greekdf["IsRoman"] = False
+    romandf["IsRoman"] = True
+    # Sort them
+    df = pd.concat([greekdf,romandf])
+    df["SortKey"] = df["Greek Entry"].apply(lambda x: unicodedata.normalize("NFD", x))
+    df = df.sort_values(by="SortKey")
+    df.drop("SortKey", axis=1, inplace=True)
+
+    df.loc[df["IsRoman"]==True,"Greek Entry"] = df.loc[df["IsRoman"]==True,"Greek Entry"].apply(
+        untag_roman_word
+    )
+    df.drop("IsRoman",axis=1, inplace=True)
+    return df
+
+
+if __name__ == "__main__":
+    greekdf, romandf = load_dfs(filePath_0, filePath_1)
+    df = sort_words(greekdf,romandf)
+    df.to_csv(f"{dir}/Merge/data/merged_GreektoHeb.csv", index=False)
